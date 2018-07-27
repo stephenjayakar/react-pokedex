d046e08988d5b8d7fe52d6fd2f6e7f2843b34735
Stephen Jayakar <aj12ay@gmail.com>
2018-07-27 16:14:49 -0700

    moved store config out of app.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,11 @@ import './App.css'
 
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import AppLayout from 'containers/AppLayout';
+import { persistor, store } from './configureStore';
 
-import rootReducer from 'utils/reducer';
-import rootSaga from 'utils/saga';
-
-const persistConfig = {
-  key: 'root',
-  storage
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
-const persistor = persistStore(store);
-sagaMiddleware.run(rootSaga);
-//persistor.purge();
 
 const App = () => (
   <Provider store={store}>
